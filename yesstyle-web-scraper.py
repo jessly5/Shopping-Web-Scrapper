16b425b552975ec950762e959dadf0e1a6ac9259
Jessica PC <jessly5@users.noreply.github.com>
2018-02-23 12:37:58 -0500

    moved urls to txt file

--- yesstyle-web-scraper.py
+++ yesstyle-web-scraper.py
@@ -10,17 +10,17 @@ referred to:
 
 https://medium.freecodecamp.org/how-to-scrape-websites-with-python-and-beautifulsoup-5946935d93fe
 https://stackoverflow.com/questions/2792650/import-error-no-module-name-urllib2
+https://stackoverflow.com/questions/11555468/how-should-i-read-a-file-line-by-line-in-python
 """
 
-chunky_pump_oxford = "https://www.yesstyle.com/en/pangmama-chunky-heel-oxfords-black-39/info.html/pid.1036382207"
-perforated_oxford = "https://www.yesstyle.com/en/colorful-shoes-perforated-oxford-pumps-black-39/info.html/pid.1035160556"
-oxford_pump = "https://www.yesstyle.com/en/pangmama-oxford-pumps/info.html/pid.1048329106"
-
-quote_page = [chunky_pump_oxford, perforated_oxford, oxford_pump]
-
+product_pages = []
 data = []
 
-for pg in quote_page:
+with open ("products.txt") as file:
+	for line in file:
+		product_pages.append(line.strip("\n"))
+	
+for pg in product_pages:
     
     # query website, return html
     page = urllib.request.urlopen(pg)
